19acc28a38355e0ea3866a3bd0710d792a918b63
Jorge Calavia <1184336+jcalavia@users.noreply.github.com>
2021-08-25 02:05:02 +0000

    New: Added initial configuration to create AKS Cluster with zero agents

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "client_id" {}
+variable "client_secret" {}
+
+variable "default_pool_agent_count" {
+    default = 0
+}
+
+#variable "ssh_public_key" {
+#    default = "~/.ssh/id_rsa.pub"
+#}
+
+variable "dns_prefix" {
+    default = "k8stest"
+}
+
+variable cluster_name {
+    default = "online-ide"
+}
+
+variable resource_group_name {
+    default = "calavia-org_online-ide"
+}
+
+variable location {
+    default = "Central US"
+}

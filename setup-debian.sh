7556596af26256892ad08d0fee058e6ea2c024c6
Jorge Calavia <1184336+jcalavia@users.noreply.github.com>
2021-08-29 19:34:07 +0000

    Fix: filename

--- setup-debian.sh
+++ setup-debian.sh
@@ -7,6 +7,7 @@ curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install terraform
 
+mkdir -p /home/codespace/.terraform.d/
 cat << EOF > /home/codespace/.terraform.d/credentials.tfrc.json
 {
   "credentials": {

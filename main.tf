93bef5e2c9133b8e9c54da9d2242ba56d1cb807f
Jorge Calavia <1184336+jcalavia@users.noreply.github.com>
2021-09-02 04:23:40 +0200

    New: Added Helm provider to install Nginx Ingress Controller (#5)

--- main.tf
+++ main.tf
@@ -50,3 +50,22 @@ module "aks" {
 
   depends_on = [module.rg]
 }
+
+provider "helm" {
+  kubernetes {
+    host                   = module.aks.kube_config.0.host
+    client_certificate     = base64decode(module.aks.kube_config.0.client_certificate)
+    client_key             = base64decode(module.aks.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(module.aks.kube_config.0.cluster_ca_certificate)
+  }
+}
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress-controller"
+  namespace  = "default"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "nginx-ingress-controller"
+  set {
+    name  = "service.type"
+    value = "LoadBalancer"
+  }
+}
